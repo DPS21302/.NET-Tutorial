c576d03fda4e9fbb326a0fa3504335951e622994
Darshit Sojitra <sojitradarshitpiyushbhai@gmail.com>
2024-05-25 14:26:36 +0530

    PostgreSQL integration

--- Books/Program.cs
+++ Books/Program.cs
@@ -1,5 +1,5 @@
-
-using Books.Services;
+using Books.DataContext;
+using Microsoft.EntityFrameworkCore;
 
 namespace Books
 {
@@ -7,31 +7,44 @@ namespace Books
     {
         public static void Main(string[] args)
         {
+            // Create a new WebApplicationBuilder instance
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
-
+            // Add controllers to the service collection
             builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-            builder.Services.AddEndpointsApiExplorer();
 
-            builder.Services.AddSingleton<BooksService>();
+            // Add support for API endpoint exploration and documentation generation
+            // with Swagger/OpenAPI
+            builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
+            // Configure PostgreSQL
+            var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+            builder.Services.AddDbContext<BooksContext>(options =>
+                options.UseNpgsql(connectionString));
+
+            // Build the WebApplication instance
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
+            // If the environment is development, enable Swagger and Swagger UI
             if (app.Environment.IsDevelopment())
             {
                 app.UseSwagger();
                 app.UseSwaggerUI();
             }
 
-            //app.UseAuthorization();
+            // Redirect HTTP requests to HTTPS
+            app.UseHttpsRedirection();
 
+            // Enable authorization middleware (typically configured later)
+            app.UseAuthorization();
 
+            // Map controllers to endpoints
             app.MapControllers();
 
+            // Run the application
             app.Run();
         }
     }
